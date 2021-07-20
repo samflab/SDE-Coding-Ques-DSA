bc233fd1f893e20b993fe32b674363fb33b4bf10
Masudha Meher <55777560+samflab@users.noreply.github.com>
2021-07-20 11:44:52 +0530

    Day 5

---
+++ Stacks-Queues/Stack with Queues.cpp
@@ -0,0 +1,58 @@
+/* Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations. */
+
+//CODE
+
+class MyStack {
+public:
+    queue <int> q;
+    /** Initialize your data structure here. */
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        q.push(x);
+        for(int i = 0; i < q.size() - 1; i++){
+            q.push(q.front());
+            q.pop();
+        } 
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+         int x = q.front(); 
+        q.pop(); 
+        return x; 
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q.size()==0;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
