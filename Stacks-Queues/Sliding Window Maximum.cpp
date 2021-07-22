3353f85fe5ed8c43ea711501490b902730cfbd2f
Masudha Meher <55777560+samflab@users.noreply.github.com>
2021-07-22 19:39:59 +0530

    Day 7

---
+++ Stacks-Queues/Sliding Window Maximum.cpp
@@ -0,0 +1,24 @@
+/* You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window. */
+//CODE
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++){
+            if(!dq.empty() && dq.front() == i -k)     //checking for out of bound for sliding window
+                dq.pop_front();
+        
+        
+            while(!dq.empty() && nums[dq.back()] <= nums[i])    //from back we check if nums are <=
+                dq.pop_back();
+            dq.push_back(i);
+            if(i >= k -1)
+                ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
